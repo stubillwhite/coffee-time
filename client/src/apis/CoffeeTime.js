fb49562a295e660381060d0a7656bf4578b22ee9
Stu W <stubillwhite@gmail.com>
2021-03-04 08:24:23 +0000

    Working on Heroku

--- client/src/apis/CoffeeTime.js
+++ client/src/apis/CoffeeTime.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+const port = process.env.NODE_ENV === 'production' ? 443 : 8080;
+const host = process.env.NODE_ENV === 'production' ? window.location.host : 'localhost';
+
 export default axios.create({
-    baseURL: 'http://localhost:8080/api'
+    baseURL: `${protocol}://${host}:${port}/api`
 });
